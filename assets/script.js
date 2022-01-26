7c4a8f9b678389c8f89769014da78fdc6c71d5e3
NumiBlue <thekindlyblog@gmail.com>
2022-01-26 17:11:03 -0600

    adjusts fields, adds variables

--- assets/script.js
+++ assets/script.js
@@ -45,9 +45,7 @@ var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
-$(".list-group").on("click", "p", function() {
-  console.log("<p> was clicked");
-});
+
 
 
 // modal was triggered
@@ -84,17 +82,57 @@ $("#task-form-modal .btn-primary").click(function() {
   }
 });
 
+// task text was clicked
+$(".list-group").on("click", "p", function() {
+  // get current text of p element
+  var text = $(this)
+    .text()
+    .trim();
+
+  // replace p element with a new textarea
+  var textInput = $("<textarea>").addClass("form-control").val(text);
+  $(this).replaceWith(textInput);
+
+  //focus new element
+  textInput.trigger("focus");
+});
+
+// editable field was un-focused
+$(".list-group").on("blur", "textarea", function() {
+  // get current value of textarea
+  var text = $(this).val();
+
+  // get status type and position in the list
+  var status = $(this)
+    .closest(".list-group")
+    .attr("id")
+    .replace("list-", "");
+  var index = $(this)
+    .closest(".list-group-item")
+    .index();
+
+    //task in array and save again to local storage
+    tasks[status][index].text = text;
+    saveTasks();
+
+//recreate p element
+var taskP = ("<p>")
+.addClass("m-1")
+.text(text);
+
+// replace textarea with new content
+  $(this).replaceWith(taskP);
+});
 
 // remove all tasks
 $("#remove-tasks").on("click", function() {
   for (var key in tasks) {
     tasks[key].length = 0;
     $("#list-" + key).empty();
-  }
+  };
   saveTasks();
 });
 
+
 // load tasks for the first time
 loadTasks();
-
-
